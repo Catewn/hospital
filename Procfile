ee3784903d5451027ab75a75524ad6df49376f01
Catherine <catherinewairimuu@gmail.com>
2022-06-18 18:21:58 +0300

    ddimng files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: python hospital.py runserver 0.0.0.0:$PORT
-web: gunicorn <best-hospital>.wsgi --log-file -
--- hospital.py
+++ hospital.py
@@ -2,6 +2,8 @@ from st_aggrid import AgGrid
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')
 
 st.title('Hospitals In Kenya Web App')
 st.markdown("""
