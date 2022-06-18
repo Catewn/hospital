915fb97f49a91e98c6b0db310f86fbac48e3e7e5
Catherine <catherinewairimuu@gmail.com>
2022-06-18 03:05:42 +0300

    Adding files

---
+++ Hospital_recommendor.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+st.sidebar.write("Select your option:")
+st.sidebar.title("Background Information")
+st.title('Hospitals In Kenya Web App')
+st.header('Hospitals Dataset')
+
+Data = pd.read_csv(r"C:\Users\User\Desktop\Hospital Recommendor\Kenya-hospitals.csv")
+print(Data.head())
+st.write(Data)
+
+st.sidebar.title("Advanced Search")
+st.sidebar.header('Facility Info')
+#Checkbox for Hospitals
+Facility_name = st.sidebar.selectbox("Select Hospital", Data["Name"].unique())
+Facility_code = st.sidebar.selectbox("Select Code", Data["Code"].unique())
+
+st.sidebar.header('Administrative unit')
+County = st.sidebar.selectbox("Select County", Data["County"].unique())
+Sub_county = st.sidebar.selectbox("Select Sub county", Data["Sub_county"].unique())
+Constituency = st.sidebar.selectbox("Select Constituency", Data["Constituency"].unique())
+Ward = st.sidebar.selectbox("Select ward", Data["Ward"].unique())
+
+st.sidebar.header('Facility Details')
+
+def user_input_features():
+    Keph_level = st.sidebar.selectbox('Keph_level', ('Level 2', 'Level 3', 'Level 4', 'Level 5', 'Level 6'))
+
+    Facility_type_category = st.sidebar.selectbox('Facility_type_category', (
+    'DISPENSARY', 'HEALTH CARE', 'HOSPITALS', 'MEDICAL CENTER', 'MEDICAL CLINIC', 'None', 'NURSING HOME', 'STAND ALONE', 'Primary healthcare services'))
+
+    Owner_type = st.sidebar.selectbox('Owner_type', ('Faith Based Organization', 'Ministry of Health', 'Non-Governmental Organizations', 'Private Practice'))
+
+    Data = {'Keph_level': [Keph_level],
+
+            'Facility_type_category': [Facility_type_category],
+
+            'Owner_type': [Owner_type]}
+
+    features = pd.DataFrame(Data)
+
+    return features
+
+input_df = user_input_features()
+
+st.sidebar.header('Availability')
+def user_input_features():
+    Open_whole_day = st.sidebar.checkbox('Open_whole_day', ('Yes', 'No'))
+
+    Open_public_holidays = st.sidebar.checkbox('Open_public_holidays', ('Yes', 'No'))
+
+    Open_weekends = st.sidebar.checkbox('Open_weekends', ('Yes', 'No'))
+
+    Open_late_night = st.sidebar.checkbox('Open_late_night', ('Yes', 'No'))
+
+    Data = {'Open_whole_day': [Open_whole_day],
+
+            'Open_public_holidays': [Open_public_holidays],
+
+            'Open_weekends': [Open_weekends],
+
+            'Open_late_night': [Open_late_night]}
+
+    features = pd.DataFrame(Data)
+
+    return features
+
+
+input_df = user_input_features()
---
+++ hospital.py
@@ -0,0 +1,55 @@
+from st_aggrid import AgGrid
+import streamlit as st
+import pandas as pd
+import altair as alt
+from itertools import cycle
+from PIL import Image
+
+st.title('Hospitals In Kenya Web App')
+st.markdown("""
+    Hospitals in Kenya web app is an application containing all health facilities in Kenya. Each health facility is identified by a code followed by details describing the geographical location, administrative location, ownership, and availability of the facility.
+""")
+st.header('Hospitals Dataset')
+st.markdown("""Data source: OpenData portal""")
+
+Data = pd.read_csv(r"C:\Users\User\Desktop\Hospital Recommendor\Kenya-hospitals.csv")
+print(Data.head())
+st.write(Data)
+
+st.title("Advanced Search")
+st.markdown("""For the advanced search we will use the filter feature to determine the best facility in an area using the most important features as per the feature importance bar chart""")
+df = pd.read_csv(r"C:\Users\User\Desktop\Hospital Recommendor\Kenya-hospitals.csv")
+AgGrid(df)
+
+st.title("Machine Learning")
+st.markdown("""A machine learning model was used to predict the best hospital in an Area. The target variable used was the Keph Level""")
+st.subheader('The six levels of health care service delivery in Kenya')
+image = Image.open('The-six-levels-of-health-care-service-delivery-in-Kenya.png')
+st.image(image, caption='Keph_levels in a nutshell')
+
+st.subheader('Keph_levels Bar Chart ')
+image = Image.open('Keph_level.png')
+st.image(image, caption='Graphical representation of hospitals in Kenya grouped by Keph_levels')
+
+st.markdown("""The graph below shows the most important features in predicting the best hospital in an area""")
+st.subheader('Feature importance')
+image = Image.open('Feature_importance.png')
+st.image(image, caption='Feature importance bar chart')
+st.markdown("""The best features in predicting the best hospital in an area include:
+
+1.Location: County
+
+2.Facility type category
+
+3.Availability :Openwhole day, open weekends
+
+4.Owner type
+""")
+
+st.subheader('Models used')
+df = pd.DataFrame({
+          'Classifier': ['KNeighborsClassifier', 'SVCclassifier', 'DecisionTreeClassifier'],
+          'Accuracy': ['0.99', '0.93', '0.87']
+  })
+
+df
